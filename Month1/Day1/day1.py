0fbdb0e1826bfa3d62960a38ad86f44caf85d746
nvsraviteja <nvsraviteja4@gmail.com>
2026-06-08 22:02:47 +0530

    Day1 code

---
+++ Month1/Day1/day1.py
@@ -0,0 +1,14 @@
+print ('Hello world')
+name = 'Ravi'
+year = 2024
+print (name)
+print (year)
+
+print ("my name is", name, "year is", year)
+
+
+bike = 'himalayan'
+trip = 'ooty'
+budget = 15000
+
+print ("I want to ride my", bike, "to", trip, "with the budget of", budget)
