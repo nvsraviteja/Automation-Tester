8938f2c88b6efae2c49b5cf5a1432f8ba95a7c5a
nvsraviteja <nvsraviteja4@gmail.com>
2026-06-12 21:40:51 +0530

    Day5

--- Month1/Day4/Day4.py
+++ Month1/Day4/Day4.py
@@ -1,14 +1,4 @@
-## Arithmetic Operators
-Add = 9+ 10
-print (Add)
-Sub = 8-5
-print (Sub)
-Mul = 5*6
-print (Mul)
-Div = 10/2
-print (Div)
-Mod = 10%3
-print (Mod)
+
 
 ## Comparison Operators
 a = 10
