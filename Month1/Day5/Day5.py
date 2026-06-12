fc528bfc780daa4cb4bdb16be79f433266c2d4aa
nvsraviteja <nvsraviteja4@gmail.com>
2026-06-12 22:20:33 +0530

    practice

---
+++ Month1/Day5/Day5.py
@@ -0,0 +1,21 @@
+
+### Task 1
+a = int(input("enter the percentage:"))
+if a < 20:
+    print ("low battery")
+# task 2
+b = int(input("enter the age:"))
+if b >= 18:
+    print ("eligible for movie")
+# task 3
+c = int(input("enter the number of KM driven on bike:"))
+if c > 3000:
+    print ("service is due")
+#task 4
+d = int(input("enter the level:"))
+if d >= 50:
+    print ("elite level")
+#task 5
+e = int(input("enter the status code:"))
+if e == 200:
+    print ("ok")
