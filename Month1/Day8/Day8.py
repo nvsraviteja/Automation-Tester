dc28793b41494353478c8c77cc69565058794d36
nvsraviteja <nvsraviteja4@gmail.com>
2026-06-15 21:20:54 +0530

    Day8 code

---
+++ Month1/Day8/Day8.py
@@ -0,0 +1,40 @@
+# task 1
+count = 1
+
+while count <= 5:
+    print (count)
+    count += 1
+
+# task 2
+count = 5
+
+while count >= 1:
+    print (count)
+    count -= 1
+
+# task 3
+
+count = 2
+
+while count <= 10:
+    print (count)
+    count += 2
+
+# task 4
+count = 1
+while count <= 5:
+    if count == 3:
+        print ("special")
+    else:
+        print (count)
+    count += 1
+
+# task 6
+count = 1
+
+while count <= 10:
+    if count == 6:
+        break
+    else:
+        print (count)
+    count += 1
