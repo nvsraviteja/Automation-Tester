c6fa9fa3e6d66c13043adcf443ffa0c476e8661a
nvsraviteja <nvsraviteja4@gmail.com>
2026-06-16 22:29:28 +0530

    day9 code

---
+++ Month1/Day9/Day9.py
@@ -0,0 +1,45 @@
+# Task 1
+
+for i in range (1,7):
+    if i == 4:
+        continue
+    print(i)
+
+# task 2
+
+for i in range (2,11,2):
+    print(i)
+
+# Task 3
+for i in range (5,0,-1):
+    print(i)
+
+# Task 4
+for i in range(1,11):
+    if i == 7:
+        break
+    print(i)
+
+# Task 5.1
+
+for i in range(1,6):
+    print (i)
+
+# Task 5.2
+
+count = 1
+while count <=5:
+    print (count)
+
+    count += 1
+
+# bonus task
+
+for i in range(1,11):
+    if i == 4:
+        continue
+    if i == 8:
+        break
+    print (f"TC{i} executed")
+
+
