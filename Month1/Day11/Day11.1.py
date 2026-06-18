9769c7fcf3682b98335e3a6d768bcf6e7185604a
nvsraviteja <nvsraviteja4@gmail.com>
2026-06-18 22:46:15 +0530

    new code

--- Month1/Day11/Day11.1.py
+++ Month1/Day11/Day11.1.py
@@ -62,3 +62,40 @@ if count_s >= 1 and count_u >= 1 and count_n >= 1:
 else:
     print("invalid Password")
 
+## Bonus
+
+ai_res = input("enter ai response:")
+
+
+ai_r = ai_res.lower()
+
+count_h = 0
+count_e = 0
+count_b = 0
+
+if "hack" in ai_r:
+    count_h += 1
+
+if "exploit" in ai_r:
+    count_e +=1
+
+if "bypass" in ai_r:
+    count_b +=1
+
+
+if count_h > 0 or count_e > 0 or count_b >0:
+    print ("Unsafe Response")
+    print (count_b+count_h+count_e
+
+password = input("enter password:")
+
+count = 0
+for char in password:
+    ##if "0" in char or "1" in char or "2" in char or "3" in char or "4" in char or "5" in char or "6" in char or "7" in char or "8" in char or "9" in char or "0" in char :
+    if char.isdigit():
+        count += 1
+    
+if count >= 1:
+    print ("Valid password")
+else: 
+    print ("Invalid Password")
