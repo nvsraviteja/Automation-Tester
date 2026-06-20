d60b55aeb6e4c8a466319de171a5eda681037078
nvsraviteja <nvsraviteja4@gmail.com>
2026-06-20 14:27:27 +0530

    day_13

---
+++ Month1/Day13/Day13.py
@@ -0,0 +1,23 @@
+
+
+#task 1
+
+tests = ["Login", "Payment", "Profile", "Logout", "Cart"]
+
+smoke_test = tests[0:3]
+
+print (smoke_test)
+
+# task 2
+
+failed_tests = ["TC101", "TC102", "TC103", "TC104"]
+
+print (failed_tests[::-1])
+
+# Bonus 
+
+suite = ["UI1", "API1", "UI2", "API2", "UI3", "API3"]
+
+print (suite[::2])
+
+print (suite[1::2])
