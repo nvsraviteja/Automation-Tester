27676b56e5541a6a29da0bea7f0abca9c936d368
nvsraviteja <nvsraviteja4@gmail.com>
2026-06-27 22:43:50 +0530

    employee1

--- Month1/Employees/Employee.py
+++ Month1/Employees/Employee.py
@@ -3,17 +3,8 @@ class Employee():
         self.name = name
         self.age = age
         self.salary = salary
-   
+        
+    def __str__(self):
+        return f"Name:{self.name}\n Age:{self.age}\nSalary:{self.salary}"
 
-# name = input("Enter name:")
-# age = int(input("Enter age:"))
-# salary = int(input("Enter Salary"))
 
-e1 = Employee("name","age","salary")
-
-class EmployeesManager():
-    def __init__(self,employee):
-        self.Employee = employee
-
-m1 = EmployeesManager(e1)
-print(m1.Employee.name)
--- Month1/Employees/Employees_Manager.py
+++ Month1/Employees/Employees_Manager.py
@@ -3,14 +3,24 @@ from Employee import Employee
 class EmployeesManager(Employee):
     def __init__(self):
         self.list_of_employees = []
+
+    def add_a_new_employee(self,name,age,salary):
+        new_employee = Employee(name,age,salary)
+        self.list_of_employees.append(new_employee)
     
+    def add_existing_employee(self,existing_employee):
+        self.list_of_employees.append(existing_employee)
 
-    def add_employee(self,name,age,salary):
-        
-
-        
-        
-        self.list_of_employees.append(Employee())
-        
+    def list_employees(self):
+        for i in self.list_of_employees:
+            print(i,"\n")
     
+    def delete_employees(self):
+        
 
+
+manager = EmployeesManager()
+nikhil = Employee("Nikhil",26,150000)
+manager.add_existing_employee(nikhil)  
+
+manager.list_employees()
