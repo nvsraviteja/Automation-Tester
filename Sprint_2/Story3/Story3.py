9db3e3807644a9f9ef28f4e0bd0bcbe34ae917e0
nvsraviteja <nvsraviteja4@gmail.com>
2026-06-30 20:30:39 +0530

    story3

--- Sprint_2/Story3/Story3.py
+++ Sprint_2/Story3/Story3.py
@@ -1,22 +1,3 @@
-# task 1
-
-import calculator
-
-ad = calculator.add(2,5)
-
-print (ad)
-
-# task 2
-
-print (calculator.add(10,20))
-
-# task 3
-
-import helper
-
-helper.greet()
-
-
 
 # task 1
 
