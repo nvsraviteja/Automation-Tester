dd499978dac4d8dbefa8dfb7410b4fee4602cb5e
nvsraviteja <nvsraviteja4@gmail.com>
2026-07-02 21:28:47 +0530

    newpro

---
+++ Sprint_2/Sprint2_project/Test_Result_Analyzer/Models/test_case.py
@@ -0,0 +1,13 @@
+
+
+class TestCase():
+    def __init__(self, data):
+        self.data = [list(d.values()) for d in data]
+
+    def validate_status():
+        pass
+    def is_failed():
+        pass
+
+    def __str__(self):
+        pass
