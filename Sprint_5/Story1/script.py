4118ea9c4d4545614641736f2d4628115bf3bda5
nvsraviteja <nvsraviteja4@gmail.com>
2026-07-11 23:38:02 +0530

    remove

--- Sprint_5/Story1/script.py
+++ Sprint_5/Story1/script.py
@@ -7,23 +7,3 @@ driver = webdriver.Chrome()
 driver.get("https://opensource-demo.orangehrmlive.com/")
 
 wait = WebDriverWait(driver, 10)
-
-wait.until(
-    EC.visibility_of_element_located((By.NAME, "username"))
-).send_keys("Admin")
-
-driver.find_element(By.NAME, "password").send_keys("admin123")
-
-driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
-
-wait.until(EC.title_contains("OrangeHRM"))
-
-actual_title = driver.title
-expected_title = "OrangeHRM"
-
-if actual_title == expected_title:
-    print("Test Passed")
-else:
-    print("Test Failed")
-
-driver.quit()
